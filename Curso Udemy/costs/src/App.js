928f1a798e97eedb54e45786d0313a327ff069b6
Gaseines <gaseinesdzn@hotmail.com>
2024-10-01 21:42:28 -0300

    Navbar, Footer, Router
    Navbar e Footer do projeto finalizadas, links para as paginas concluídas

--- Curso Udemy/costs/src/App.js
+++ Curso Udemy/costs/src/App.js
@@ -3,28 +3,27 @@ import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 
 import Container from './components/layouts/Container';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newProject">Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/company" element={<Company />} />
           <Route path="/newProject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
