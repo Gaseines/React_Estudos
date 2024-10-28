5bc68fb50990dacd33531f0bebe8ce80734a9a1e
Gaseines <gaseinesdzn@hotmail.com>
2024-10-28 19:12:39 -0300

    Criando a area de Projetos

--- Curso Udemy/costs/src/components/pages/Projects.js
+++ Curso Udemy/costs/src/components/pages/Projects.js
@@ -1,16 +1,38 @@
 import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 import Message from "../layouts/Message"
 import styles from './Project.module.css'
 import LinkButton from "../layouts/LinkButton"
 import Container from "../layouts/Container"
+import ProjectCard from "../project/ProjectCard"
+
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then((res) =>
+            res.json()
+        ).then((data) => {
+            console.log(data)
+            setProjects(data)
+        }).catch((err) => {
+            console.log(err)
+        });
+    }, [])
 
     return (
         <div className={styles.project_container} >
@@ -21,7 +43,16 @@ function Projects() {
             {message && <Message type="sucess" msg={message} />}
 
             <Container customClass="start">
-                Projetos...
+                {projects.length > 0 &&
+                    projects.map((project) =>
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />)}
+
             </Container>
         </div>
     )
